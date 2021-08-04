508a65b39f193009f0a92b64a279e63ced540821
idembele70 <idembele70@gmail.com>
2021-08-04 19:06:31 +0200

    settings

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
 import { Grid, useMediaQuery, useTheme } from "@material-ui/core"
+import React from "react"
 import PokemonCard from "./PokemonCard/PokemonCard"
 import useStyles from "./style"
+
 function MainContent() {
   const classes = useStyles()
   const theme = useTheme()
   const isSmallDisplay = useMediaQuery(theme.breakpoints.down("xs"))
   return (
-    <Grid container spacing={isSmallDisplay ? 0 : 4} className={classes.root} justifyContent="center">
+    <Grid
+      container
+      spacing={isSmallDisplay ? 0 : 4}
+      className={classes.root}
+      justifyContent="center"
+    >
       <Grid item>
         <PokemonCard />
       </Grid>
